c6662227ed2ce99309bbc2e38038996ae4166b04
santiago <santiarias1984@gmail.com>
2022-03-20 16:14:55 -0300

    removing harcoded nft address

--- pages/api/nftbalance.js
+++ pages/api/nftbalance.js
@@ -5,13 +5,14 @@ const NFT_BALANCE_ENDPOINT_URL =
 
 const accessKey = process.env.ACCESS_KEY;
 const chainId = "0x61" // BSC testnet
+const nftAddress = ""
 
 export default async function handler(req, res) {
   const { userId } = req.query;
   const wallet = await client.get(userId);
   const params = new URLSearchParams({
     wallet,
-    nftAddress: "0xf009f8d7fb3d5934da6ddbe0e5a4695f5f4c1e53",
+    nftAddress,
     chainId,
   });
 
