0c551e34f612ddbdd82aecb0c3496321cd536e16
santiago <santiarias1984@gmail.com>
2022-03-20 16:09:22 -0300

    improve nft balance response

--- pages/api/nftbalance.js
+++ pages/api/nftbalance.js
@@ -1,42 +1,33 @@
-import client from "./utils/redisClient"
+import client from "./utils/redisClient";
 
-const NFT_BALANCE_ENDPOINT_URL = "https://api.blockonelabs.com/api/nft/balance?"
+const NFT_BALANCE_ENDPOINT_URL =
+  "https://api.blockonelabs.com/api/nft/balance?";
+
+const accessKey = process.env.ACCESS_KEY;
+const chainId = "0x61" // BSC testnet
 
 export default async function handler(req, res) {
-
-  const {userId} = req.query
-  const wallet = await client.get(userId)
+  const { userId } = req.query;
+  const wallet = await client.get(userId);
   const params = new URLSearchParams({
-     wallet,
+    wallet,
     nftAddress: "0xf009f8d7fb3d5934da6ddbe0e5a4695f5f4c1e53",
-    chainId:"0x61"
-})
-const accessKey = process.env.ACCESS_KEY
-
+    chainId,
+  });
 
   const response = await fetch(NFT_BALANCE_ENDPOINT_URL + params, {
-    method: 'GET',
+    method: "GET",
     headers: {
-        'x-api-key': accessKey
-      },
+      "x-api-key": accessKey,
+    },
   });
-  const result = await response.json();
-  console.log(result)
-  res.status(200).json(result);
-
-
-
-//   const response = await fetch("http://example.com/api/endpoint")
-  
-
-//   .catch(function (err) {
-//     console.log("Unable to fetch -", err);
-//   });
-
-
-//   res.status(200).json({
-//     walletAddress: walletAddress || "NOT FOUND",
-//     userId: userId || "NOT FOUND"
-//   });
+  const nftBalanceResponse = await response.json();
+  const nftBalance = nftBalanceResponse.result.map(nft => {
+      const attributes = JSON.parse(nft.metadata).attributes
+      return {
+          id: nft.token_id,
+          attributes
+      }
+  })
+  res.status(200).json(nftBalance);
 }
-
