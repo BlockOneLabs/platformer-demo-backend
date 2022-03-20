26b8272fe2b007b56e6b50516a3af4129bb4664e
santiago <santiarias1984@gmail.com>
2022-03-20 20:56:33 -0300

    cleanup

--- pages/api/nftbalance.js
+++ pages/api/nftbalance.js
@@ -4,10 +4,11 @@ const NFT_BALANCE_ENDPOINT_URL =
   "https://api.blockonelabs.com/api/nft/balance?";
 
 const accessKey = process.env.ACCESS_KEY;
-const chainId = "0x61" // BSC testnet
-const nftAddress = ""
+const chainId = "0x61"; // BSC testnet
+const nftAddress = "";
 
 export default async function handler(req, res) {
+
   const { userId } = req.query;
   const wallet = await client.get(userId);
   const params = new URLSearchParams({
@@ -23,16 +24,24 @@ export default async function handler(req, res) {
     },
   });
   const nftBalanceResponse = await response.json();
-  console.log("nftBalanceResponse")
-  console.log(JSON.stringify(nftBalanceResponse))
-  const nftBalance = nftBalanceResponse.map(nft => {
-      const name = nft.name
-      const tokenAddress = nft.tokenAddress
-      return {
-          id: nft.id,
-          name,
-          tokenAddress
-      }
-  })
+  console.log("nftBalanceResponse");
+  console.log(JSON.stringify(nftBalanceResponse));
+  const nftBalance = nftBalanceResponse.map((nft) => {
+    const name = nft.name;
+    const tokenAddress = nft.tokenAddress;
+    return {
+      id: nft.id,
+      name,
+      tokenAddress,
+    };
+  });
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-api-key"
+  );
+  
   res.status(200).json(nftBalance);
 }
